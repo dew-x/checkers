dcd4a237966aef682c6fc8886422c9571cadeb40
aFigueras <andreu.figueras@gmail.com>
2016-06-01 17:32:57 +0200

    basic board functionallity

--- vs/Game/Board.cpp
+++ vs/Game/Board.cpp
@@ -4,9 +4,82 @@
 
 Board::Board()
 {
+	reset();
 }
 
 
 Board::~Board()
 {
+}
+
+Player Board::currentPlayer()
+{
+	return (Player)(turn%PLAYER_NONE);
+}
+
+int Board::currentTurn()
+{
+	return turn;
+}
+
+bool Board::gameEnded()
+{
+	return piecesBlack == 0 || piecesWhite == 0;
+}
+
+Player Board::whoWon()
+{
+	if (piecesBlack == 0) return PLAYER_WHITE;
+	else if (piecesWhite == 0) return PLAYER_BLACK;
+	else return PLAYER_NONE;
+}
+
+bool Board::isMoveValid(Position a, Position b)
+{
+	unsigned aID = pos2id(a);
+	unsigned bID = pos2id(b);
+	if (playerUsePiece(currentPlayer(), grid[aID]) && grid[bID] == NONE) {
+		return true;
+	}
+}
+
+void Board::makeMove(Position a, Position b)
+{
+	if (isMoveValid(a, b)) {
+		unsigned aID = pos2id(a);
+		unsigned bID = pos2id(b);
+		grid[bID] = grid[aID];
+		grid[aID] = NONE;
+		if (b.y == 0 || b.y == 7) grid[bID] = (Piece)(grid[bID]|QUEEN);
+	}
+}
+
+void Board::getGrid(GRID & res)
+{
+	for (unsigned i = 0; i < GSIZE; ++i) res[i] = grid[i];
+}
+
+void Board::reset()
+{
+	turn = 0;
+	piecesBlack = 12;
+	piecesWhite = 12;
+	for (unsigned i = 0; i < GSIZE; ++i) grid[i] = DEFAULT[i];
+}
+
+unsigned Board::pos2id(Position p)
+{
+	return (p.x / 2)*p.y;
+}
+
+Position Board::id2pos(unsigned id)
+{
+	unsigned y = id / 8;
+	unsigned x = (id % 4)*2+(y%2);
+	return { x,y };
+}
+
+bool Board::playerUsePiece(Player player, Piece piece)
+{
+	return (piece&WHITE && player == PLAYER_WHITE) || (piece&BLACK && player == PLAYER_BLACK);
 }
--- vs/Game/Board.h
+++ vs/Game/Board.h
@@ -1,8 +1,63 @@
 #pragma once
+#include <vector>
+using namespace std;
+
+enum Piece {
+	NONE=0,
+	WHITE=1,
+	BLACK=2,
+	QUEEN = 4,
+	WHITEQUEEN = 5,
+	BLACKQUEEN = 6
+};
+
+enum Player {
+	PLAYER_WHITE,
+	PLAYER_BLACK,
+	PLAYER_NONE,
+};
+
+struct Position {
+	unsigned x, y;
+};
+
+const unsigned GSIZE = 32;
+
+typedef Piece GRID[32];
+
+const GRID DEFAULT = {
+	WHITE,WHITE,WHITE,WHITE,
+	WHITE,WHITE,WHITE,WHITE,
+	WHITE,WHITE,WHITE,WHITE,
+	NONE,NONE,NONE,NONE,
+	NONE,NONE,NONE,NONE,
+	BLACK,BLACK,BLACK,BLACK,
+	BLACK,BLACK,BLACK,BLACK,
+	BLACK,BLACK,BLACK,BLACK,
+};
+
 class Board
 {
 public:
 	Board();
 	~Board();
+	Player currentPlayer();
+	int currentTurn();
+	bool gameEnded();
+	Player whoWon();
+	bool isMoveValid(Position a, Position b);
+	void makeMove(Position a, Position b);
+	void getGrid(GRID &res);
+	void reset();
+private:
+	unsigned pos2id(Position p);
+	Position id2pos(unsigned id);
+	bool playerUsePiece(Player player, Piece piece);
+private:
+	int turn;
+	GRID grid;
+	int piecesWhite;
+	int piecesBlack;
+
 };
 
